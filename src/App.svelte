398ba52e444a47320ca7e9679062f95f2ebf082c
sabrina <sabrinaabrodi20@gmail.com>
2022-10-30 20:05:48 +0100

    first ommit (New portfolio coming soon!)

--- src/App.svelte
+++ src/App.svelte
@@ -4,9 +4,31 @@
 </script>
 
 <main>
-<h1>hshd</h1>
+<h1>Sabrina Abrodi</h1>
+<p>New portfolio coming soon!</p>
 </main>
 
 <style>
+main {
+  display: flex;
+  align-self: center;
+  background-color: #100d2f;
+  height: 100vh;
+  width: 100vw;
+  flex-direction: column;
+align-items: center;
+justify-content: center;
+font-family: 'Poppins', sans-serif;
+color: aqua;
+  }
 
+  h1 {
+  font-family: "Bebas Neue", cursive;
+  color: rgb(223, 221, 246);
+  }
+
+  p {
+      color: rgb(81, 77, 121);
+      font-style: italic;
+  }
 </style>
